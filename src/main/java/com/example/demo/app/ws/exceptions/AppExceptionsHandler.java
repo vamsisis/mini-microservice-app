3c1ee61f9271a46e7d83c4fc0b678d3afd12e21f
vamsi siddineni <svamsi.nz@gmail.com>
2021-04-17 23:13:11 -0400

    added Dependency injections & Costructer based Dependecny injection

--- src/main/java/com/example/demo/app/ws/exceptions/AppExceptionsHandler.java
+++ src/main/java/com/example/demo/app/ws/exceptions/AppExceptionsHandler.java
@@ -1,3 +1,4 @@
+package com.example.demo.app.ws.exceptions;
 
 import java.util.Date;
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.example.demo.app.ws.exceptions.UserServiceException;
 import com.example.demo.ui.model.response.ErrorMessage;
 
 @ControllerAdvice
@@ -25,4 +27,20 @@ public class AppExceptionsHandler extends ResponseEntityExceptionHandler {
 		return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
 
 	}
+	
+	
+	@ExceptionHandler(value = { NullPointerException.class , UserServiceException.class  })
+	public ResponseEntity<Object> handleSpecificNullPointerExceptions(Exception ex, WebRequest request) {
+		
+		String errorMessageDescription = ex.getLocalizedMessage();
+		if(errorMessageDescription ==null) errorMessageDescription = ex.toString();
+		
+		ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getLocalizedMessage());
+
+		return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+
+	}
+	
+	
+	
 }
---
+++ src/main/java/com/example/demo/app/ws/exceptions/UserServiceException.java
@@ -0,0 +1,15 @@
+package com.example.demo.app.ws.exceptions;
+
+public class UserServiceException extends RuntimeException{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -69739508641706301L;
+
+	public  UserServiceException(String message)
+	{
+		super(message);
+	}
+
+}
